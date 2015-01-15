e7072d065fcf316b956eef69741c2928103ccf29
Yi Ding <yi.ding.cn@outlook.com>
2015-01-15 13:54:07 +0800

    Clean up

--- app/routes.php
+++ app/routes.php
@@ -28,40 +28,5 @@ Route::get('/', function()
 	return $array;
 });
 
-// Route::get('/Products', function()
-// {
-// 	$table = DB::table('products')->get();
-// 	$result = array(
-// 		"@odata.context" => Config::get("app.url")."/\$metadata#"."Products",
-// 		"value" => $table
-// 		);
-
-//     return $result;
-// });
-
-// Route::get('/Products({id})', function($id)
-// {
-// 	$table = DB::table('products')->where("ID",$id)->get();
-// 	$result = array(
-// 		"@odata.context" => Config::get("app.url")."/\$metadata#"."Products"."/\$entity",
-// 		"value" => $table
-// 		);
-
-//     return $result;
-// });
-
-// Route::delete('/Products({id})', function($id)
-// {
-// 	$item = DB::table('products')->where("ID",$id)->get();
-// 	if(empty($item))
-// 	{
-// 		return Response::make(NULL, 404);
-// 	}
-// 	else
-// 	{
-// 		$status = DB::table('products')->delete($id);
-// 		return Response::make(NULL, 204);
-// 	}
-// });
 
 
